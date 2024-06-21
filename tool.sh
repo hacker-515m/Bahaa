c32f27b7a1888abb0a63eb61a2611214e3aa1d7e
hacker-515m <bahaa0106400@gmail.com>
2024-06-21 11:51:26 +0300

    Add files via upload

---
+++ tool.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt install python3
+pip install colorama
+git clone https://github.com/Cyber-Dioxide/CyberPhish
+cd CyberPhish
+ls
+pip install -r requirements.txt
+python3 CyberPhish.py
